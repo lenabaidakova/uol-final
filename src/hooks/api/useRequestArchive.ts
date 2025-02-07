64c0c5aa9dbbf24cbbf8380db6de9fcdf0d1e8d9
e.baidakova <lena.baydakova@gmail.com>
2025-02-07 14:09:21 +0400

    Request archive hook

--- src/hooks/api/useRequestArchive.ts
+++ src/hooks/api/useRequestArchive.ts
@@ -1,28 +1,22 @@
 import { useMutation } from '@tanstack/react-query';
 import apiClient from '@/lib/axios';
 import type { MutationOptions } from '@/types/Api';
-import { RequestType, UrgencyType } from '@/constants/Request';
 
 export type UpdateRequestData = {
-  title: string;
-  dueDate: string;
-  details: string;
-  location: string;
-  type: RequestType;
-  urgency: UrgencyType;
+  id: string;
 };
 
-const createRequest = (data: UpdateRequestData): Promise<unknown> => {
-  return apiClient.patch('/requests/update', data);
+const archiveRequest = (data: UpdateRequestData): Promise<unknown> => {
+  return apiClient.patch('/requests/archive', data);
 };
 
-export function useRequestUpdate(
+export function useRequestArchive(
   opt?: MutationOptions<unknown, UpdateRequestData>
 ) {
   const { mutate, isPending, isError, error } = useMutation({
-    mutationFn: createRequest,
+    mutationFn: archiveRequest,
     meta: {
-      name: 'Update request',
+      name: 'Archive request',
     },
     ...opt,
   });
